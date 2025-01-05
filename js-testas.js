b93cd764b0c8e16597cf63bbe6ba4bf034c70d3a
Rolandas5 <r.bendorius@gmail.com>
2025-01-05 19:30:20 +0200

    add js-testas.js

--- js-testas.js
+++ js-testas.js
@@ -5,15 +5,15 @@ Pritaikykite savo sukurtą funkciją ir išspausdinkite atitikmenį
 doleriais.
 */
 
-// function convertToDollars(euros, exchange = 1.0389) {
-//   return euros * exchange;
-// }
+function convertToDollars(euros, exchange = 1.0389) {
+  return euros * exchange;
+}
 
-// const euros = Math.floor(Math.random() * 1000) + 1;
+const euros = Math.floor(Math.random() * 1000) + 1;
 
-// const dollars = convertToDollars(euros);
+const dollars = convertToDollars(euros);
 
-// console.log(`${euros} eurų atitinka ${dollars.toFixed(2)} dolerių.`);
+console.log(`${euros} eurų atitinka ${dollars.toFixed(2)} dolerių.`);
 
 //
 /*
@@ -23,15 +23,15 @@ Pritaikykite savo sukurtą funkciją ir išspausdinkite atitikmenį
 eurais.
 */
 
-// function convertToEuros(dollars, exchange = 1.0389) {
-//   return dollars / exchange;
-// }
+function convertToEuros(dollars, exchange = 1.0389) {
+  return dollars / exchange;
+}
 
-// const dollars = Math.floor(Math.random() * 1000) + 1;
+const dollars = Math.floor(Math.random() * 1000) + 1;
 
-// const euros = convertToEuros(dollars);
+const euros = convertToEuros(dollars);
 
-// console.log(`${dollars} dolerių atitinka ${euros.toFixed(2)} eurų.`);
+console.log(`${dollars} dolerių atitinka ${euros.toFixed(2)} eurų.`);
 
 //
 /*
@@ -45,28 +45,28 @@ Normalu = 18.5 <= BMI < 25
 Per mažas svoris = BMI < 18.5
 */
 
-// function calculateBmi(weight, height) {
-//   const bmi = weight / height ** 2;
-//   let bodyCondition;
+function calculateBmi(weight, height) {
+  const bmi = weight / height ** 2;
+  let bodyCondition;
 
-//   if (bmi < 18.5) {
-//     bodyCondition = 'Per mažas svoris';
-//   } else if (bmi >= 18.5 && bmi < 25) {
-//     bodyCondition = 'Normalu';
-//   } else {
-//     bodyCondition = 'Viršsvoris';
-//   }
+  if (bmi < 18.5) {
+    bodyCondition = 'Per mažas svoris';
+  } else if (bmi >= 18.5 && bmi < 25) {
+    bodyCondition = 'Normalu';
+  } else {
+    bodyCondition = 'Viršsvoris';
+  }
 
-//   return { bmi: bmi.toFixed(2), bodyCondition };
-// }
+  return { bmi: bmi.toFixed(2), bodyCondition };
+}
 
-// const weight = 83;
-// const height = 1.7;
+const weight = 83;
+const height = 1.7;
 
-// const result = calculateBmi(weight, height);
+const result = calculateBmi(weight, height);
 
-// console.log(`Jūsų kūno masės indeksas (KMI): ${result.bmi}`);
-// console.log(`Kūno būklė: ${result.bodyCondition}`);
+console.log(`Jūsų kūno masės indeksas (KMI): ${result.bmi}`);
+console.log(`Kūno būklė: ${result.bodyCondition}`);
 
 //
 /*
@@ -75,28 +75,28 @@ pasakytų kiek tai yra sekundėmis, minutėmis, valandomis,
 dienomis.
 */
 
-// function convertAgeInTime(ageInYears) {
-//   const secondsInMinute = 60;
-//   const secondsInHour = 3600;
-//   const secondsInDay = 86400;
-//   const secondsInYear = 31536000;
+function convertAgeInTime(ageInYears) {
+  const secondsInMinute = 60;
+  const secondsInHour = 3600;
+  const secondsInDay = 86400;
+  const secondsInYear = 31536000;
 
-//   const seconds = ageInYears * secondsInYear;
-//   const minutes = seconds / secondsInMinute;
-//   const hours = seconds / secondsInHour;
-//   const days = seconds / secondsInDay;
+  const seconds = ageInYears * secondsInYear;
+  const minutes = seconds / secondsInMinute;
+  const hours = seconds / secondsInHour;
+  const days = seconds / secondsInDay;
 
-//   return { seconds, minutes, hours, days };
-// }
+  return { seconds, minutes, hours, days };
+}
 
-// const ageInYears = 42;
-// const result = convertAgeInTime(ageInYears);
+const ageInYears = 42;
+const result = convertAgeInTime(ageInYears);
 
-// console.log(`Amžius: ${ageInYears} metai`);
-// console.log(`Sekundės: ${result.seconds}`);
-// console.log(`Minutės: ${result.minutes}`);
-// console.log(`Valandos: ${result.hours}`);
-// console.log(`Dienos: ${result.days}`);
+console.log(`Amžius: ${ageInYears} metai`);
+console.log(`Sekundės: ${result.seconds}`);
+console.log(`Minutės: ${result.minutes}`);
+console.log(`Valandos: ${result.hours}`);
+console.log(`Dienos: ${result.days}`);
 
 //
 /*
@@ -104,22 +104,22 @@ dienomis.
 duomenis iš Farenheito į Celsijų, ir atvirkščiai.
 */
 
-// function convertFahrenheitToCelsius(fahrenheit) {
-//   return ((fahrenheit - 32) * 5) / 9;
-// }
+function convertFahrenheitToCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
 
-// function convertCelsiusToFahrenheit(celsius) {
-//   return (celsius * 9) / 5 + 32;
-// }
+function convertCelsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
 
-// const fahrenheit = 100;
-// const celsius = 37.78;
+const fahrenheit = 100;
+const celsius = 37.78;
 
-// const resultCelsius = convertFahrenheitToCelsius(fahrenheit);
-// const resultFahrenheit = convertCelsiusToFahrenheit(celsius);
+const resultCelsius = convertFahrenheitToCelsius(fahrenheit);
+const resultFahrenheit = convertCelsiusToFahrenheit(celsius);
 
-// console.log(`${fahrenheit}°F = ${resultCelsius.toFixed(2)}°C`);
-// console.log(`${celsius}°C = ${resultFahrenheit.toFixed(2)}°F`);
+console.log(`${fahrenheit}°F = ${resultCelsius.toFixed(2)}°C`);
+console.log(`${celsius}°C = ${resultFahrenheit.toFixed(2)}°F`);
 
 //
 /*
@@ -128,15 +128,15 @@ duomenis iš Farenheito į Celsijų, ir atvirkščiai.
 dešimties neturėtų būti brūkšniuko.
 */
 
-// let number = '';
-// for (let i = 1; i <= 10; i++) {
-//   if (i === 10) {
-//     number += i;
-//   } else {
-//     number += i + '-';
-//   }
-// }
-// console.log(number);
+let number = '';
+for (let i = 1; i <= 10; i++) {
+  if (i === 10) {
+    number += i;
+  } else {
+    number += i + '-';
+  }
+}
+console.log(number);
 
 //
 /*
@@ -148,26 +148,26 @@ dešimties neturėtų būti brūkšniuko.
 *****
 */
 
-// for (let i = 1; i <= 5; i++) {
-//   let stars = '';
-//   for (let j = 1; j <= i; j++) {
-//     stars += '*';
-//   }
-//   console.log(stars);
-// }
+for (let i = 1; i <= 5; i++) {
+  let stars = '';
+  for (let j = 1; j <= i; j++) {
+    stars += '*';
+  }
+  console.log(stars);
+}
 
 //
 /*
 8. Parašykite kodą, kuris apskaičiuos kiek liko dienų iki Kalėdų.
 */
 
-// let today = new Date();
-// let christmas = new Date(today.getFullYear(), 11, 25);
+let today = new Date();
+let christmas = new Date(today.getFullYear(), 11, 25);
 
-// let timeDifference = christmas - today;
-// let daysLeft = Math.ceil(timeDifference / (1000 * 3600 * 24));
+let timeDifference = christmas - today;
+let daysLeft = Math.ceil(timeDifference / (1000 * 3600 * 24));
 
-// console.log(`Iki Kalėdų liko ${daysLeft} dienų.`);
+console.log(`Iki Kalėdų liko ${daysLeft} dienų.`);
 
 //
 /*
@@ -177,13 +177,13 @@ Tomas,Dainius,Paulius,Jonas
 Tomas+Dainius+Paulius+Jonas
 */
 
-// let names = ['Tomas', 'Dainius', 'Paulius', 'Jonas'];
+let names = ['Tomas', 'Dainius', 'Paulius', 'Jonas'];
 
-// let firstTest = names.join(',');
-// console.log(firstTest);
+let firstTest = names.join(',');
+console.log(firstTest);
 
-// let secondTest = names.join('+');
-// console.log(secondTest);
+let secondTest = names.join('+');
+console.log(secondTest);
 
 //
 /*
@@ -193,47 +193,47 @@ mažoji raidė, skaičius, specialusis simbolis. Visi slaptažodžio
 simboliai privalo būti atsitiktiniai ir atsitiktine tvarka.
 */
 
-// function passwordGenerator(len) {
-//   // didžiosios raidės
-//   const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//   // mažosios raidės
-//   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-//   // skaičiai
-//   const number = '0123456789';
-//   // simboliai
-//   const punctuation = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
-//   // visi simboliai
-//   const allChars = upperCase + lowerCase + number + punctuation;
+function passwordGenerator(len) {
+  // didžiosios raidės
+  const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // mažosios raidės
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  // skaičiai
+  const number = '0123456789';
+  // simboliai
+  const punctuation = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+  // visi simboliai
+  const allChars = upperCase + lowerCase + number + punctuation;
 
-//   let password = '';
+  let password = '';
 
-//   // parenkama didžioji raidė
-//   password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
-//   // parenkama mažoji raidė
-//   password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
-//   // parenkamas skaičius
-//   password += number.charAt(Math.floor(Math.random() * number.length));
-//   // parenkamas simbolis
-//   password += punctuation.charAt(
-//     Math.floor(Math.random() * punctuation.length)
-//   );
+  // parenkama didžioji raidė
+  password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+  // parenkama mažoji raidė
+  password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+  // parenkamas skaičius
+  password += number.charAt(Math.floor(Math.random() * number.length));
+  // parenkamas simbolis
+  password += punctuation.charAt(
+    Math.floor(Math.random() * punctuation.length)
+  );
 
-//   // užpildo atsitiktiniais simboliais iš visų kategorijų
-//   while (password.length < len) {
-//     password += allChars.charAt(Math.floor(Math.random() * allChars.length));
-//   }
+  // užpildo atsitiktiniais simboliais iš visų kategorijų
+  while (password.length < len) {
+    password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+  }
 
-//   // suformuoja slaptažodį su simboliais atsitiktine tvarka
-//   password = password
-//     .split('')
-//     .sort(() => Math.random() - 0.5)
-//     .join('');
+  // suformuoja slaptažodį su simboliais atsitiktine tvarka
+  password = password
+    .split('')
+    .sort(() => Math.random() - 0.5)
+    .join('');
 
-//   // užtikrina, kad slaptažodis atitiktų nurodytą ilgį
-//   return password.substr(0, len);
-// }
+  // užtikrina, kad slaptažodis atitiktų nurodytą ilgį
+  return password.substr(0, len);
+}
 
-// console.log(passwordGenerator(12));
+console.log(passwordGenerator(12));
 
 //
 //
